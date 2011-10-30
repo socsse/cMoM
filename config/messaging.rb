55d7bd543e5f6a8c474d941e7a3cc16fade74fbd
Ken Joyner <Ken@KJoyner.com>
2011-10-30 13:13:58 -0700

    Modified message queue to seperate name of queue based on rails environment

--- config/messaging.rb
+++ config/messaging.rb
@@ -3,10 +3,18 @@
 # can also be used to configure filters, and processor groups
 #
 ActiveMessaging::Gateway.define do |s|
+
   #s.destination :orders, '/queue/Orders'
   #s.filter :some_filter, :only=>:orders
   #s.processor_group :group1, :order_processor
 
-  s.destination :job_queue, 'CMoMJobQueue'
+  case Rails.env
+    when 'production'
+      s.destination :job_queue, 'CMoMJobQueue'
+    when 'test'
+      s.destination :job_queue, '/queue/CMoMJobQueue'
+    else
+      s.destination :job_queue, '/queue/CMoMJobQueue'
+    end
 
 end
