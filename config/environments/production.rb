82ec42d8c5b0fa9117afc6b3e6997659ccec5075
Ken Joyner <Ken@KJoyner.com>
2012-01-13 18:18:11 -0800

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ CMoM::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile = '*.js'
+  config.assets.precompile << '*_pc.js'
 
   # ActionMailer Config, no errors will be raised
   #
